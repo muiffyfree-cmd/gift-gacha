fbcdf5648575ca881dd2166298e4fe105c6b6d9d
muiffy <muiffy.free@gmail.com>
2026-06-29 19:32:31 +0900

    AdSense所有権確認コードを生scriptタグで<head>に直接埋め込み

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,11 +34,11 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <head>
-        <Script
+        {/* eslint-disable-next-line @next/next/no-sync-scripts */}
+        <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4177080531198906"
           crossOrigin="anonymous"
-          strategy="afterInteractive"
         />
         {ADSENSE_CLIENT_ID && (
           <Script
